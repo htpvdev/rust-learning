7892b212908f781505a2d870284900ca85f67a76
htpvdev <haruki20211121@gmail.com>
2024-03-21 23:56:29 +0900

    lesson3 variables completed.

--- lesson_3/variables/src/main.rs
+++ lesson_3/variables/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    const MAX_POINTS: u32 = 100_000;
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    // addition
+    // 足し算
+    let sum = 5 + 10;
+
+    // subtraction
+    // 引き算
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    // 掛け算
+    let product = 4 * 30;
+
+    // division
+    // 割り算
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+                         // 結果は0
+
+    // remainder
+    // 余り
+    let remainder = 43 % 5;
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';    //ハート目の猫
 }
